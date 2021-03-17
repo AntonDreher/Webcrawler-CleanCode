64babd197059f53058699b6033597cfe886be207
Martin Unterhuber <m.unterhuber99@gmail.com>
2021-03-17 10:37:27 +0100

    Added/Implemented Page and PageStats classes

---
+++ src/main/java/com/cleancode/webcrawler/Page.java
@@ -0,0 +1,54 @@
+package com.cleancode.webcrawler;
+
+import org.jsoup.helper.HttpConnection;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class Page {
+    private URL url;
+    private Document document;
+    private PageStats stats;
+
+    public Page(URL url) throws IOException {
+        this.url = url;
+        getDocument();
+        this.stats = new PageStats(document);
+    }
+
+    private URL getAbsoluteUrl(String uri){
+        try {
+            if (new URI(uri).isAbsolute()){
+                return new URL(uri);
+            } else {
+                return new URL(url, uri);
+            }
+        } catch (MalformedURLException | URISyntaxException e) {
+            return null;
+        }
+    }
+
+    public Set<URL> getLinkedUrls(){
+        Elements links = document.select(PageStats.LINK_TAG + PageStats.EXCLUDE_SAME_PAGE_LINKS);
+        List<String> uris = links.eachAttr("href");
+        return uris.stream().map(this::getAbsoluteUrl).filter(Objects::nonNull).collect(Collectors.toSet());
+    }
+
+    private void getDocument() throws IOException {
+        this.document = HttpConnection.connect(url).get();
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s: %s", url.toString(), stats.toString());
+    }
+}
---
+++ src/main/java/com/cleancode/webcrawler/PageStats.java
@@ -0,0 +1,59 @@
+package com.cleancode.webcrawler;
+
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.util.StringTokenizer;
+
+public class PageStats {
+    private Document document;
+    private int words;
+    private int links;
+    private int images;
+    private int videos;
+
+    final static String LINK_TAG = "a";
+    final static String EXCLUDE_SAME_PAGE_LINKS = ":not([href^=#])";
+    final static String IMAGE_TAG = "img";
+    final static String VIDEO_TAG = "video";
+
+    public PageStats(Document document){
+        this.document = document;
+        setStats();
+    }
+
+    private void setStats(){
+        countWords();
+        countImages();
+        countLinks();
+        countVideos();
+    }
+
+    private void countWords(){
+        String text = document.text();
+        StringTokenizer tokenizer = new StringTokenizer(text);
+        words = tokenizer.countTokens();
+    }
+
+    private int countTagElements(String tag){
+        Elements elements = document.select(tag);
+        return elements.size();
+    }
+
+    private void countLinks(){
+        this.links = countTagElements(LINK_TAG + EXCLUDE_SAME_PAGE_LINKS);
+    }
+
+    private void countImages(){
+        this.images = countTagElements(IMAGE_TAG);
+    }
+
+    private void countVideos(){
+        this.videos = countTagElements(VIDEO_TAG);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s words, %s links, %s images, %s videos", words, links, images, videos);
+    }
+}
